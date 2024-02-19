bb27f1abb677f0a1dd8391a27f4352a38b7024c1
Julianna <yanietorres13@gmail.com>
2024-02-19 12:39:35 +0800

    added file

--- mvc/app/core/App.php
+++ mvc/app/core/App.php
@@ -2,8 +2,26 @@
 
 class App
 {
+    protected $controller = 'Home';
 public function __consturct()
 {
-sdadasdasbingot
+    $url = $this->splitURL();
+
+    if(isset($url[0])); {
+        if(file_exists('../app/controllers/' . ucfirst($url[0]) . '.php')) {
+
+            $this->controller = ucfirst($url[0]);
+            unset($url[0]);
+        }
+    }
+}
+
+private function splitURL()
+{
+if(isset($_GET['url'])){
+
+    $url = explode("/",trim($_GET['url'],"/"));
+    return $url;
+    }
 }
 }
