2f0f395c43ad72204454d50dfc4593b7fb564334
Julianna <yanietorres13@gmail.com>
2024-03-18 11:59:34 +0800

    dasdasd

--- mvc/app/controllers/Home.php
+++ mvc/app/controllers/Home.php
@@ -4,9 +4,9 @@ class Home extends Controller
 {
     public function index()
     {
-        $model = new Model();
-        $arr['lastname'] = 'Cabatingan';
-        $data = $model->where($arr);
+        $user = new User();
+        $arr['firstname'] = 'Clyde';
+        $data = $user->where($arr);
         show ($data);
        $this->view('home');
     }
